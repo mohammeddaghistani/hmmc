50a88b7e6bb20d16c07f1298c61d5b1bce7f1dbe
mohammeddaghistani <mdsa2003@gmail.com>
2026-01-21 02:13:46 +0300

    Update municipal_lease_types.py

--- modules/municipal_lease_types.py
+++ modules/municipal_lease_types.py
@@ -1,10 +1,22 @@
 class MunicipalLeaseTypes:
-    """تعريب أنواع التأجير البلدية بالكامل"""
+    """إدارة أنواع التأجير البلدية باللغة العربية"""
     def __init__(self):
         self.lease_types = {
-            'TEMPORARY_ACTIVITY': {'name': 'تأجير مؤقت للأنشطة والفعاليات', 'multiplier': 'mult_temp'},
-            'LONG_TERM_INVESTMENT': {'name': 'تأجير طويل الأجل (استثماري)', 'multiplier': 'mult_long'},
-            'DIRECT_LEASE': {'name': 'تأجير مباشر (المادة 27)', 'multiplier': 'mult_direct'}
+            'TEMPORARY_ACTIVITY': {
+                'name': 'تأجير مؤقت للأنشطة والفعاليات',
+                'multiplier_key': 'mult_temporary',
+                'max_period': '6 أشهر'
+            },
+            'LONG_TERM_INVESTMENT': {
+                'name': 'تأجير طويل الأجل (استثماري)',
+                'multiplier_key': 'mult_long_term',
+                'max_period': '50 سنة'
+            },
+            'DIRECT_LEASE': {
+                'name': 'تأجير مباشر (المادة 27)',
+                'multiplier_key': 'mult_direct',
+                'max_period': 'حسب الحالة'
+            }
         }
     def get_lease_options(self):
         return {k: v['name'] for k, v in self.lease_types.items()}
