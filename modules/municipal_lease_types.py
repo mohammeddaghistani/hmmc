f603ad2eb5f169d22c9dcd610fe15d5ec49f45bb
mohammeddaghistani <mdsa2003@gmail.com>
2026-01-21 01:48:59 +0300

    Update municipal_lease_types.py

--- modules/municipal_lease_types.py
+++ modules/municipal_lease_types.py
@@ -1,23 +1,30 @@
+import streamlit as st
+
 class MunicipalLeaseTypes:
-    """أنواع التأجير البلدية المعربة بالكامل حسب اللوائح"""
+    """إدارة أنواع التأجير البلدية المعربة بالكامل"""
+    
     def __init__(self):
         self.lease_types = {
             'TEMPORARY_ACTIVITY': {
-                'name': 'تأجير مؤقت (فعاليات/أنشطة)',
-                'multiplier_key': 'mult_temporary',
-                'description': 'عقود قصيرة الأجل تصل إلى 6 أشهر'
+                'name': 'تأجير مؤقت للأنشطة والفعاليات',
+                'multiplier_key': 'mult_temp',
+                'duration': '6 أشهر قابلة للتمديد'
             },
             'LONG_TERM_INVESTMENT': {
                 'name': 'تأجير استثماري طويل الأجل',
-                'multiplier_key': 'mult_long_term',
-                'description': 'عقود استثمارية تصل إلى 50 سنة'
+                'multiplier_key': 'mult_long',
+                'duration': 'حتى 50 سنة'
             },
             'DIRECT_LEASE': {
-                'name': 'تأجير مباشر',
+                'name': 'تأجير مباشر (المادة 27)',
                 'multiplier_key': 'mult_direct',
-                'description': 'تأجير بدون منافسة حسب حالات المادة 27'
+                'duration': 'حسب شروط الحالة'
             }
         }
 
-    def get_all_types_arabic(self):
-        return {k: v['name'] for k, v in self.lease_types.items()}
+    def render_lease_type_selection(self):
+        st.subheader("🏛️ اختيار نوع التأجير البلدي")
+        options = {k: v['name'] for k, v in self.lease_types.items()}
+        selected_key = st.selectbox("نوع التأجير المطلوب", options=list(options.keys()), format_func=lambda x: options[x])
+        st.info(f"الوصف: {self.lease_types[selected_key]['duration']}")
+        return selected_key
