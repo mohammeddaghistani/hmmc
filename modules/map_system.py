dfc38d095a607188936e19806ab768e6fb096471
mohammeddaghistani <mdsa2003@gmail.com>
2026-01-21 02:49:45 +0300

    Create map_system.py

---
+++ modules/map_system.py
@@ -0,0 +1,9 @@
+# modules/map_system.py
+import streamlit as st
+
+class MapSystem:
+    def __init__(self):
+        pass
+
+    def render_map(self):
+        st.info("نظام الخرائط قيد التطوير...")
